fed602f96bece732d749002fa9c1e96cb4b3d83f
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-07-25 15:13:24 -0300

    Updated testing: removed users module

--- src/testing/testing-module.ts
+++ src/testing/testing-module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from '../users/user-schema';
+
 import { TestingController } from './testing-controller';
 import { TestingService } from './testing-service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    //MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
     // Add other models here
   ],
   controllers: [TestingController],
--- src/testing/testing-service.ts
+++ src/testing/testing-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from '../users/user-schema';
+import { User, UserDocument } from '../users/users.schema';
 
 @Injectable()
 export class TestingService {
