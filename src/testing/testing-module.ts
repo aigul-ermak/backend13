eb68112c72c6fdf365f56c53cc6c465944773e2c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-08-02 18:22:06 -0300

    Updated testing module: delete testing data

--- src/testing/testing-module.ts
+++ src/testing/testing-module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { TestingController } from './testing-controller';
 import { TestingService } from './testing-service';
 import { Blog, BlogsSchema } from '../blogs/blogs.schema';
+import { PostsSchema } from '../posts/posts.schema';
+import { User, UsersSchema } from '../users/users.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogsSchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostsSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
   ],
   controllers: [TestingController],
   providers: [TestingService],
